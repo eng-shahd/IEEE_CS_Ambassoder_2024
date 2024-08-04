752e5bd8a51c4a22cf344db0177e0d8a1f2912b3
Shahd Mahmoud <158595641+eng-shahd@users.noreply.github.com>
2024-08-04 22:49:17 +0300

    Create A.RoundHouse.cpp

---
+++ Rookies_Tasks/Task2/A.RoundHouse.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    int final_position = (a + b - 1 + n) % n + 1;
+
+    cout << final_position << endl;
+    
+    return 0;
+}
